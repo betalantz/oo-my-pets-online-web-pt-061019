b73da7d333137760f95cbb1b35fa329664d85535
Lantz Warrick <betalantz@gmail.com>
2019-07-15 21:48:12 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
-  attr_accessor :mood
+  attr_accessor :mood, :owner
   attr_reader :name
   @@all = []
   def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
     self.class.all << self
     owner.cats << self
   end
