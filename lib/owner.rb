e3be07c02e913a34d878886cc6e7f9d37884c4a8
Lantz Warrick <betalantz@gmail.com>
2019-07-15 21:50:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,8 +36,10 @@ class Owner
   end
 
   def sell_pets
-    @dogs.each {|dog| dog.mood = "nervous", dog.owner = nil}
-    @cats.each {|cat| cat.mood = "nervous", cat.owner = nil}
+    @dogs.each {|dog| dog.mood = "nervous"
+      dog.owner = nil}
+    @cats.eachg {|cat| cat.mood = "nervous"
+      cat.owner = nil}
     @cats.clear
     @dogs.clear
   end
