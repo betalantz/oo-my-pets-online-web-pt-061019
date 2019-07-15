dd129820e5cf2989be1cc478257d36a4f54ff76c
Lantz Warrick <betalantz@gmail.com>
2019-07-15 21:48:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
-  attr_accessor :mood
+  attr_accessor :mood, :owner
   attr_reader :name
   @@all = []
   def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
     self.class.all << self
     owner.dogs << self
   end
@@ -12,5 +13,5 @@ class Dog
   def self.all
     @@all
   end
-  
+
 end
